f57f2bbfdb25c54397e5b63e017259a8f9512d17
Andrei See <coding@andrei-see.com>
2024-07-09 15:57:39 +0200

    [Feature] Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,26 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Col from 'react-bootstrap/Col';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import Dropdown from 'react-bootstrap/Dropdown';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Index from './pages/Index';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button variant="primary">Primary</Button>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Dropdown Button
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </>
+    <Container>
+      <Row className="flex-column min-vh-100 gap-3">
+        <Col as="header" className="text-center flex-grow-0 p-3">
+          <Header />
+        </Col>
+        <Col as="main" className="text-center bg-light p-3">
+          <Index />
+        </Col>
+        <Col as="footer" className="text-center flex-grow-0 p-3">
+          <Footer />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
