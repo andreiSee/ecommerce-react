dc47862f05e856c22ee8386849ead0306c315b4d
Andrei See <coding@andrei-see.com>
2024-08-05 19:37:20 +0200

    [Refactor] Extract products component

---
+++ src/components/products.tsx
@@ -0,0 +1,33 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import products from '../../data/products.json';
+import AddCartItem from '../components/AddCartItem';
+
+function Products() {
+  return (
+    <>
+      <h2>Produkte</h2>
+      <Row sm={1} md={2} lg={3}>
+        {products &&
+          products.map((product) => (
+            <Col className="g-4 mw-200" key={product.id}>
+              <Card as="article" className="h-100">
+                <Card.Img variant="top" src={`/img/${product.img}`} />
+                <Card.Body>
+                  <Card.Title as="h3">{product.name}</Card.Title>
+                  <div>
+                    {product.price} {product.currency}
+                  </div>
+                  <Card.Text>{product.description}</Card.Text>
+                  <AddCartItem product={product} />
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </>
+  );
+}
+
+export default Products;
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,10 +1,8 @@
-import Card from 'react-bootstrap/Card';
-import products from '../../data/products.json';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import AddCartItem from '../components/AddCartItem';
-import Cart from '../components/Cart';
+import Row from 'react-bootstrap/Row';
 import App from '../App';
+import Cart from '../components/Cart';
+import Products from '../components/products';
 
 function IndexPage() {
   return (
@@ -14,25 +12,7 @@ function IndexPage() {
           <Cart />
         </Col>
         <Col sm={8} md={9} className="mt-3 mt-sm-0">
-          <h2>Produkte</h2>
-          <Row sm={1} md={2} lg={3}>
-            {products &&
-              products.map((product) => (
-                <Col className="g-4 mw-200" key={product.id}>
-                  <Card as="article" className="h-100">
-                    <Card.Img variant="top" src={`/img/${product.img}`} />
-                    <Card.Body>
-                      <Card.Title as="h3">{product.name}</Card.Title>
-                      <div>
-                        {product.price} {product.currency}
-                      </div>
-                      <Card.Text>{product.description}</Card.Text>
-                      <AddCartItem product={product} />
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-          </Row>
+          <Products />
         </Col>
       </Row>
     </App>
