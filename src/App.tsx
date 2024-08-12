76f52e209a617a4c23446b50700c55596e8b7e56
Andrei See <coding@andrei-see.com>
2024-08-12 17:52:49 +0200

    [Feature] Styling

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,12 @@ function App({ children }: { children: React.ReactNode }) {
         <Col as="header" className="flex-grow-0 pb-4">
           <Header />
         </Col>
-        <Col as="main" className="text-center bg-light p-3 rounded-4">
+        <Col as="main" className="text-center bg-light p-4 rounded-4">
           {children}
         </Col>
         <Col
           as="footer"
-          className="flex-grow-0 d-flex flex-column flex-wrap gap-3 align-content-center p-3"
+          className="flex-grow-0 d-flex flex-column flex-wrap gap-3 align-content-center m-5"
         >
           <Footer />
         </Col>
